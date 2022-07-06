000cd93ec3a0b107061bc542bc766a177b052084
SAYANDEEP ADHIKARY <93661020+sayandeep-adhikary@users.noreply.github.com>
2022-07-06 11:48:04 +0530

    Add files via upload

---
+++ pattern1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter Row Number : ";
+    cin >> n;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout << i ;
+    //         j = j + 1;
+    //     }
+    //     cout << "\n";
+    //     i++;
+    // }
+    for (int i = 1; i <= n; i++)
+    {
+        int k=1;
+        for (int j = 1; j <= (2*n); j++)
+        {
+            if (j>=(n+2-i) && j<=(n-1+i))
+            {
+                cout << "* ";
+                if(j==n) 
+                    k--;
+                
+            }
+            else {
+                cout<<k<<" " ;
+                if(j<n) k++;
+                else if(j>n) k--;   
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
